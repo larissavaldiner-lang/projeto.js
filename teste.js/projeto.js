991fbd3e7f60a643cd2624790afd1079c84930f4
larissa <larissavaldiner@gmail.com>
2025-08-28 19:53:42 -0300

    Add files via upload

---
+++ teste.js/projeto.js
@@ -0,0 +1,14 @@
+// importar no início
+const readline = require('readline-sync');
+
+// exemplo de uso
+const nome = readline.question("Digite seu nome: ");
+console.log("Olá,", nome);
+
+// função menu
+function menu() {
+  console.log("\n[1] Cadastrar  [2] Listar  [0] Sair");
+  return readline.question("> ").trim();
+}
+
+console.log("Opção escolhida:", menu());
